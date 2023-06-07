8cb0481c009de31a5ee1ecd9aa9639a8926aa3ec
Jennypgalvez <jennypiril@gmail.com>
2023-06-06 21:34:56 -0600

    Complete the prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,6 +5,13 @@ class Program
     static void Main(string[] args)
     {
         // This is prep 1
-        Console.WriteLine("Hello Prep1 World!");
+        //Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        String FirstName = Console.ReadLine();
+        // Prompt for their last name 
+        Console.Write("What is your last name? ");
+        String LastName = Console.ReadLine();
+        // Display the text back
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
