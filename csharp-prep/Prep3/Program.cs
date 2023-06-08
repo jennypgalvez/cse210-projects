b3be8401e924f9bdfc8b5691d2fe4df4d8d68eb0
Jennypgalvez <jennypiril@gmail.com>
2023-06-08 11:43:00 -0600

    prep 3 assignment and 1 strech challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {  
+
+        /*Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());*/
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+        int guessCount = 0;
+
+        while (guess != magicNumber)
+        { 
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount = guessCount + 1;
+
+                
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");              
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        Console.WriteLine($"It took you {guessCount} guesses. ");
+        Console.Write("Would you like to play again (yes/no)? ");
     }
 }
+
