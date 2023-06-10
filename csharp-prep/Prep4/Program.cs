2434d665acfe473eba0cca152ba7219810e0ad64
Jennypgalvez <jennypiril@gmail.com>
2023-06-10 10:33:53 -0600

    List and sum prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int userNumber = -1;
+
+        while (userNumber != 0) 
+        {
+            
+            Console.Write("Enter number: ");
+            String userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum} ");
+
+    }   
 }
+
+
+        
