bb068e53c918b26943edb3864a8684cee1a3c1e0
Jennypgalvez <jennypiril@gmail.com>
2023-06-10 12:36:41 -0600

    last part prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -29,6 +29,21 @@ class Program
         }
         Console.WriteLine($"The sum is: {sum} ");
 
+        float everage = ((float)sum) / numbers.Count;
+
+        int max = numbers [0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+           
+        }
+        Console.WriteLine($"The max is: {max}. " );
+
+
     }   
 }
 
